6ef52d2b648e84baa50e360556171ed0c6642464
TC Tobin-Campbell <tobalsemail@gmail.com>
2024-08-28 20:59:57 -0600

    Fix colorscheme for vscode

--- init.lua
+++ init.lua
@@ -876,20 +876,18 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- init = function()
-    --   -- Load the colorscheme here.
-    --   -- Like many other themes, this one has different styles, and you could load
-    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --   vim.cmd.colorscheme 'tokyonight-moon'
-    --
-    --   -- You can configure highlights by doing something like:
-    --   vim.cmd.hi 'Comment gui=none'
-    -- end,
     'Mofiqul/vscode.nvim',
-    priority = 1000,
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      --   -- Load the colorscheme here.
+      --   -- Like many other themes, this one has different styles, and you could load
+      --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      --   vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'vscode'
       vim.o.background = 'dark'
+      --
+      --   -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
