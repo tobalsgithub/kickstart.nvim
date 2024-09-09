4c6018294b8cf50ecfa22eb13ae0e26ea6d24dd3
TC Tobin-Campbell <tobalsemail@gmail.com>
2024-09-08 21:16:16 -0600

    use vim-tmux-navigator plugin instead of manually mapping keys

--- init.lua
+++ init.lua
@@ -203,10 +203,10 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Override escape key in insert mode
 vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Escape' })
@@ -263,6 +263,7 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  { 'christoomey/vim-tmux-navigator' },
   -- Find and replace across files
   { 'nvim-pack/nvim-spectre' },
   -- Multiple cursors
